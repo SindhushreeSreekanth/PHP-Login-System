c6e121897e0ac32ee686d2ab88a2a6f682522a33
SindhushreeSreekanth <sindhusreekanth17@gmail.com>
2020-11-24 20:14:23 +0530

    Added registration via ajax

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ $(document)
 		return false;
 	} else if(dataObj.password.length < 8){
 		_error
-			.text("Please enter a valid email address")
+			.text("Please enter a valid password")
 			.show();
 		return false;
 	}
@@ -23,17 +23,71 @@ $(document)
 
 	$.ajax({
 		type: 'POST',
-		url: 'ajax/register.php',
+		url: 'php_login_course/ajax/register.php',
 		data: dataObj,
 		dataType: 'json',
 		async: true,
 	})
 	.done(function ajaxDone(data) {
-		console.log(data);
+		// console.log(data);
 		if(data.redirect !== undefined){
 			window.location = data.redirect;
+		}else if(data.error !== undefined){
+			_error
+				.text(data.error)
+				.show();
 		}
-		alert(data.name);
+		// alert(data.name);
+	})
+	.fail(function ajaxFailed(e) {
+		console.log(e);
+	})
+	.always(function ajaxAlwaysDoThis(data) {
+		console.log('Always');
+	})
+	return false;
+})
+
+//Login
+.on("submit", "form.js-login", function(event){
+	event.preventDefault();
+
+	var _form = $(this);
+	var _error = $(".js-error", _form);
+
+	var dataObj = {
+		email: $("input[type='email']", _form).val(),
+		password : $("input[type='password']", _form).val()
+	}
+	if(dataObj.email.length < 6){
+		_error.text("Please enter a valid email address").show();
+		return false;
+	} else if(dataObj.password.length < 8){
+		_error
+			.text("Please enter a valid password")
+			.show();
+		return false;
+	}
+
+	_error.hide();
+
+	$.ajax({
+		type: 'POST',
+		url: 'php_login_course/ajax/login.php',
+		data: dataObj,
+		dataType: 'json',
+		async: true,
+	})
+	.done(function ajaxDone(data) {
+		// console.log(data);
+		if(data.redirect !== undefined){
+			window.location = data.redirect;
+		}else if(data.error !== undefined){
+			_error
+				.text(data.error)
+				.show();
+		}
+		// alert(data.name);
 	})
 	.fail(function ajaxFailed(e) {
 		console.log(e);
--- inc/config.php
+++ inc/config.php
@@ -8,6 +8,7 @@
 
 	//Include the DB.php file
 	include_once "classes/DB.php";
+	include_once "classes/Filter.php";
 
 	$con = DB::getConnection();
 ?>
